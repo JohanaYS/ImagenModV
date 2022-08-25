c502836f84358357f8d3d497acdf4ba7cb7737e3
Josue Guardado <jguardadosv@gmail.com>
2022-08-25 13:55:54 -0600

    usandoDTO

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
-import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 import { Usuario } from './entities/usuario.entity';
 import * as bcrypt from 'bcrypt';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
